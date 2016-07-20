1e5df8368457a2b26dd83c61497ab8b7ab7f4519
liaoyuhuan35 <562898419@qq.com>
2016-07-20 14:39:29 +0800

    [Subject] 1 沉浸式状态栏，添加自定义ActionBar 2 移除不需要的floatmenu 3 修改设置项 [Bug Number/New Feature] NA [Ripple Effect] NA [Solution] NA [Project]
    	modified:   app/app.iml
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/com/scout/musicplayer/MusicActivity.java
    	new file:   app/src/main/res/drawable-xxhdpi/ic_menu.png
    	new file:   app/src/main/res/drawable-xxhdpi/ic_menu_search.png
    	new file:   app/src/main/res/drawable/music_tab_bar_text_color_selector.xml
    	new file:   app/src/main/res/drawable/play_page_back_selector.xml
    	modified:   app/src/main/res/layout/activity_music.xml
    	modified:   app/src/main/res/layout/app_bar_music.xml
    	new file:   app/src/main/res/layout/include_music_tab_bar.xml
    	modified:   app/src/main/res/layout/nav_header_music.xml
    	deleted:    app/src/main/res/values-v21/styles.xml
    	modified:   app/src/main/res/values/colors.xml
    	modified:   app/src/main/res/values/strings.xml
    	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/scout/musicplayer/MusicActivity.java
+++ app/src/main/java/com/scout/musicplayer/MusicActivity.java
@@ -1,43 +1,37 @@
 package com.scout.musicplayer;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MusicActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private void setSystemBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // LOLLIPOP 解决方案
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // KITKAT瑙ｅ喅鏂规
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
+        setSystemBarTransparent();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -58,16 +52,6 @@ public class MusicActivity extends AppCompatActivity
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -90,3 +74,11 @@ public class MusicActivity extends AppCompatActivity
         return true;
     }
 }
+
+//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
+//
+//    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//drawer.setDrawerListener(toggle);
+//        toggle.syncState();
