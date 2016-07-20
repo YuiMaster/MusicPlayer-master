8910a79180b3311e1c4a36010d8557f62fbc1908
liaoyuhuan35 <562898419@qq.com>
2016-07-20 11:06:55 +0800

    [Subject] 界面调整 [Bug Number/New Feature] NA [Ripple Effect] NA [Solution] NA [Project]
    	modified:   app/app.iml
    	modified:   app/src/main/java/com/scout/musicplayer/MusicActivity.java
    	new file:   app/src/main/res/drawable-xxhdpi/ic_menu_about.png
    	new file:   app/src/main/res/drawable-xxhdpi/ic_menu_exit.png
    	new file:   app/src/main/res/drawable-xxhdpi/ic_menu_settings.png
    	deleted:    app/src/main/res/drawable/ic_menu_camera.xml
    	deleted:    app/src/main/res/drawable/ic_menu_gallery.xml
    	deleted:    app/src/main/res/drawable/ic_menu_slideshow.xml
    	modified:   app/src/main/res/layout/app_bar_music.xml
    	modified:   app/src/main/res/layout/content_music.xml
    	modified:   app/src/main/res/layout/nav_header_music.xml
    	modified:   app/src/main/res/menu/activity_music_drawer.xml
    	modified:   app/src/main/res/values/colors.xml
    	modified:   app/src/main/res/values/dimens.xml

--- app/src/main/java/com/scout/musicplayer/MusicActivity.java
+++ app/src/main/java/com/scout/musicplayer/MusicActivity.java
@@ -58,20 +58,16 @@ public class MusicActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -79,13 +75,9 @@ public class MusicActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_exit) {
 
         } else if (id == R.id.nav_share) {
 
